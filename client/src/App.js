ea51e546bace83e9546f886fa04b0bb49ff924e4
grace179 <72989367+grace179@users.noreply.github.com>
2020-12-02 01:40:53 +0900

    add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import LadingPage from './components/views/LandinngPage/LadingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route exact path="/" component={LadingPage}/>
+        <Route path="/login" component={LoginPage}/>
+        <Route path="/register" component={RegisterPage}/>
+      </Switch>
     </div>
+  </Router>
+
   );
 }
 
---
+++ client/src/components/views/LandinngPage/LadingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LadingPage() {
+  return (
+    <div>
+      Landing
+    </div>
+  )
+}
+
+export default LadingPage;
