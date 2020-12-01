00607945e72ad67d700b60e9db47acde0a328265
grace179 <72989367+grace179@users.noreply.github.com>
2020-12-01 21:45:46 +0900

    login

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -37,7 +38,7 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre('save', function( next ){
 
-  let user = this;
+  const user = this;
 
   if(user.isModified('password')){
     // 비밀번호 암호화
@@ -45,8 +46,8 @@ userSchema.pre('save', function( next ){
       if(err) return next(err)
 
       bcrypt.hash(user.password, salt, function(err, hash){
-        if(err) return next(err)
-        user.password = hash
+        if(err) return next(err);
+        user.password = hash;
         next()
 
       })
@@ -56,6 +57,31 @@ userSchema.pre('save', function( next ){
   }
   
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+
+  // plainPassword , 암호화된 비밀번호 같은지 체크
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+    if(err) return cb(err);
+    cb(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(cb){
+  
+  const user = this;
+
+  // jsonwebtoken 으로 토큰 생성
+  const token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+  user.token = token;
+
+  user.save(function(err, user){
+    if(err) return cb(err);
+    cb(null, user)
+  })
+}
+
 
 const User = mongoose.model('User', userSchema);
 
